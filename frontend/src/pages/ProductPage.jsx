7edcfd8faad52e6154939965824a715a0e755b10
JaataLamine <alamine1409@gmail.com>
2024-05-26 13:13:07 +0000

    Fetch products from the server

--- frontend/src/pages/ProductPage.jsx
+++ frontend/src/pages/ProductPage.jsx
@@ -1,12 +1,22 @@
 import { useParams, Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
 import { MdArrowBackIos } from "react-icons/md";
+import axios from "axios";
 import Rating from "../components/Rating";
-import products from "../products";
 
 const ProductPage = () => {
+  const [product, setProduct] = useState({});
   const { id: productId } = useParams();
-  const product = products.find((p) => p._id === productId);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [productId]);
 
   return (
     <>
