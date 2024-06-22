6d8c8ad108ff7f1257c7e8634c9282efb9618cc4
JaataLamine <alamine1409@gmail.com>
2024-06-22 18:18:13 +0000

    Create user routes and handle user validation

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./config/db.js";
-import productRoutes from "./routes/productRoute.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 const port = 5000;
@@ -9,11 +10,16 @@ connectDB(); // Connect to MongoDB
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
