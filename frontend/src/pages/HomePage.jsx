304cfd921df5e262a3aaa905d2ee5dcac06eb184
JaataLamine <alamine1409@gmail.com>
2024-08-17 17:38:44 +0000

    Search Box component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import { Row, Col } from "react-bootstrap";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Product from "../components/Product";
 import { useGetProductsQuery } from "../slices/productsApiSlice";
 import Loader from "../components/Loader";
@@ -16,6 +16,11 @@ const HomePage = () => {
 
   return (
     <>
+      {keyword && (
+        <Link to="/" className="btn btn-light mb-3">
+          Go Back
+        </Link>
+      )}
       {isLoading ? (
         <Loader />
       ) : error ? (
